52d9c6080275e798a5e315464c4649a1bc3893ce
Sun ju Kim <ksunju0610@naver.com>
2020-02-22 23:40:57 +0900

    재귀함수

---
+++ src/algorithm/recursive/Q1_10872.java
@@ -0,0 +1,38 @@
+package algorithm.recursive;
+
+import java.util.Scanner;
+
+public class Q1_10872 {
+
+	public static void main(String[] args) {
+
+	/*
+		문제
+		0보다 크거나 같은 정수 N이 주어진다. 이때, N!을 출력하는 프로그램을 작성하시오.
+		
+		입력
+		첫째 줄에 정수 N(0 ≤ N ≤ 12)가 주어진다.
+		
+		출력
+		첫째 줄에 N!을 출력한다.
+		
+		예제 입력 1
+		10
+		예제 출력 1
+		3628800
+	*/
+		
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		int sum = 1;
+		for(int i = num; i > 0; i--) {
+			sum *= i;
+		}
+		System.out.println(sum);
+		
+		
+		
+		
+	}
+
+}
